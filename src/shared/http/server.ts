604b3757e46e0e0c1bd03ef45ec964438f9abfd3
evanzs <evandrofidk@hotmail.com>
2022-01-30 18:40:26 -0300

    feat: error handler

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,26 @@
-import express from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import ErrorsHandler from '@shared/errors/ErrorsHandler';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ErrorsHandler) {
+      response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({ status: error, message: error.message });
+  },
+);
 
 app.listen(3030, () => {
   console.log('Server started port 3030');
